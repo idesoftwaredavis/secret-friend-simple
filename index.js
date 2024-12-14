dba6b01d1384f84475057f0a626c2e29749afd26
davis <davis@MacBook-Air-de-davis.local>
2024-12-14 01:25:03 -0300

    se agrega mensaje y validacion de participantes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 //DOM
 const button = document.getElementById("chooseButton");
+const btnOpciones = document.getElementById("btnOpciones");
 const result = document.getElementById("result");
 const musicaContainer = document.getElementById("musica");
 const imagenContainer = document.getElementById("imagen-container");
 
 const img = document.createElement("img");
 const audio = document.createElement("audio");
+const buttonRepeat = document.createElement("button");
+
 
 let count = 0;
+
 
 // Family
 const participantes = [
   { nombre: "Davis", avatar: "assets/img/davis.png" },
-  { nombre: "Cony", avatar: "assets/img/cony.png" },
   { nombre: "Tomas", avatar: "assets/img/tomas.png" },
   { nombre: "Diego", avatar: "assets/img/diego.png" },
   { nombre: "Kevin", avatar: "assets/img/kevin.png" },
@@ -50,12 +53,34 @@ const chooseSecretFriend = () => {
   imagenContainer.append(img);
   // Deshabilitar el botón una vez seleccionado
   button.disabled = true;
+  button.style.backgroundColor = "black";
+
+  buttonRepeat.classList.add("m-3");
+  buttonRepeat.style.backgroundColor = "green";
+  buttonRepeat.textContent = "Repetir";
+  btnOpciones.append(buttonRepeat);
 
   audio.src = `${chosenMusica}`;
   musicaContainer.append(audio);
-  audio.playbackRate += 0.5;
+  audio.playbackRate += 0.3;
   audio.play();
+
+  if(participantes.length == 0){
+    buttonRepeat.remove();
+    // Crear un nuevo elemento
+    const nuevoElemento = document.createElement('h3');
+    nuevoElemento.textContent = 'Te volviste re loc@ relaja esa venita. Este fue el último';
+    nuevoElemento.classList.add("alert");
+    nuevoElemento.classList.add("alert-danger");
+
+    button.replaceWith(nuevoElemento);
+
+  }
 };
+
+
+
 
 // Eventos
 button.addEventListener("click", chooseSecretFriend);
+buttonRepeat.addEventListener("click", chooseSecretFriend);
